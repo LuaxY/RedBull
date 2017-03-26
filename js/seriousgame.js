95ea3b838580a1f716e817d41d9588cc0cdeeef8
tiad <timothee.adrien@gmail.com>
2017-03-26 22:27:05 +0200

    step 1 positionnement

--- js/seriousgame.js
+++ js/seriousgame.js
@@ -1,5 +1,18 @@
 
-$( document ).ready(function() {
+/*
+    Début - Pour le dev du jeu
+ */
+   localStorage.setItem('sexe', 'garcon');
+/*
+    Fin - Pour le dev du jeu
+ */
 
+$( document ).ready(function() {
+    // Set image étudiant
+    $('#etudiant').attr('src', 'img/' + localStorage.getItem('sexe') + '.png');
+
+    $($('.progress-bar')[0]).attr('aria-valuenow', 75).css('width', 75 + '%').append(75 + '%');
+    $($('.progress-bar')[1]).attr('aria-valuenow', 100).css('width', 100 + '%').append(100 + '%');
+    $($('.progress-bar')[2]).attr('aria-valuenow', 100).css('width', 100 + '%').append(100 + '%');
 });
 
