8c759670acbedff4a13014f293f4992942beccd6
tiad <timothee.adrien@gmail.com>
2017-03-27 09:41:22 +0200

    step 2

--- js/seriousgame.js
+++ js/seriousgame.js
@@ -14,5 +14,6 @@ $( document ).ready(function() {
     $($('.progress-bar')[0]).attr('aria-valuenow', 75).css('width', 75 + '%').append(75 + '%');
     $($('.progress-bar')[1]).attr('aria-valuenow', 100).css('width', 100 + '%').append(100 + '%');
     $($('.progress-bar')[2]).attr('aria-valuenow', 100).css('width', 100 + '%').append(100 + '%');
+    $($('.progress-bar')[4]).attr('aria-valuenow', 1).css('width', 1 + '%').append(0);
 });
 
