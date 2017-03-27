635bb24081c284b707780eb50ac0afd38f375021
FannyLAJ <fanny.lajeun@gmail.com>
2017-03-27 14:30:12 +0200

    Affichage

--- js/seriousgame.js
+++ js/seriousgame.js
@@ -14,6 +14,7 @@ $( document ).ready(function() {
     $($('.progress-bar')[0]).attr('aria-valuenow', 75).css('width', 75 + '%').append(75 + '%');
     $($('.progress-bar')[1]).attr('aria-valuenow', 100).css('width', 100 + '%').append(100 + '%');
     $($('.progress-bar')[2]).attr('aria-valuenow', 100).css('width', 100 + '%').append(100 + '%');
+    $($('.progress-bar')[3]).attr('aria-valuenow', 25).css('width', 25 + '%').append(25 + '%');
     $($('.progress-bar')[4]).attr('aria-valuenow', 1).css('width', 1 + '%').append(0);
 });
 
